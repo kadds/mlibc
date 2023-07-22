181617f553925fcc0bca4201ab7b52c6899a29da
kadds <itmyxyf@gmail.com>
2023-07-22 19:06:04 +0800

    feat: naos release 4.0

---
+++ abis/naos/auxy.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define AT_NULL 0
+#define AT_PHDR 3
+#define AT_PHENT 4
+#define AT_PHNUM 5
+#define AT_ENTRY 9
+#define AT_SECURE 23
+#define AT_RANDOM 25
+#define AT_EXECFN 31
+#define AT_SYSINFO_EHDR 33
